03312d842297da070a43245c9e23833880dd7f60
LordSapling <55261860+LordSapling@users.noreply.github.com>
2019-10-03 18:12:41 -0500

    Created a moving ramp support that moves with player input

---
+++ Ice Cold Beer Unity Project/Assets/Scripts/MovingRampSupport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRampSupport : MonoBehaviour
+{
+    private float moveSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test");
+        // transform.Translate(5, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the player presses up arrow, we move the square up.
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, moveSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -moveSpeed, 0);
+        }
+        // Debug.Log("UPDATE METHOD");
+    }
+}
