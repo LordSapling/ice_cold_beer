ee832de655184c8e4970139a6853a66dfad498db
LordSapling <55261860+LordSapling@users.noreply.github.com>
2019-12-15 15:50:48 -0600

    Added restart key and new obstical

---
+++ Ice Cold Beer Unity Project/Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && GameOver.IsGameOver)
+        {
+            GameOver.IsGameOver = false;
+            // This depends on your scene being named exactly "Game Scene" case sensitive
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
